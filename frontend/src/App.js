5f1d04a401ca94a8f85ef7c439ff5e1eb64638dc
Aneesh-Pothuru <54965062+Aneesh-Pothuru@users.noreply.github.com>
2020-08-27 18:35:41 -0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import "./App.css";
+import NavBar from "./components/navBar/navBar";
+//012C36
+//01BFFF
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
